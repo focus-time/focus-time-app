40a1360613e64978dd0bfdea16e6702c1e7128b8
MShekow <s2mashek@uni-bonn.de>
2023-06-10 09:21:32 +0200

    fix: configuration on macOS

--- focus_time_app/command_execution/impl/macos_command_executor.py
+++ focus_time_app/command_execution/impl/macos_command_executor.py
@@ -10,7 +10,6 @@ from focus_time_app.command_execution.abstract_command_executor import CommandEx
 
 
 class MacOsCommandExecutor(AbstractCommandExecutor):
-    FOCUS_MODE_SHORTCUT_FILENAME = "focus-time-app.shortcut"
 
     def execute_commands(self, commands: List[str], dnd_profile_name: str):
         for command in commands:
@@ -37,7 +36,7 @@ class MacOsCommandExecutor(AbstractCommandExecutor):
 
     @staticmethod
     def _is_shortcut_installed() -> bool:
-        output_bytes = subprocess.check_output(["shortcuts", "list"], shell=True)
+        output_bytes = subprocess.check_output(["/usr/bin/shortcuts", "list"])
         output_lines = output_bytes.decode("utf-8").splitlines()
         return CommandExecutorConstants.MACOS_FOCUS_MODE_SHORTCUT_NAME in output_lines
 
@@ -47,9 +46,11 @@ class MacOsCommandExecutor(AbstractCommandExecutor):
                      "Once you press Enter, a dialog opens in which you have to press the 'Add Shortcut' button.",
                      default='', prompt_suffix='\n')
         if getattr(sys, 'frozen', False):
-            path = str(Path(sys.executable).parent / self.FOCUS_MODE_SHORTCUT_FILENAME)
+            path = str(Path(sys.executable).parent
+                       / f"{CommandExecutorConstants.MACOS_FOCUS_MODE_SHORTCUT_NAME}.shortcut")
         else:
-            path = str(Path(__file__).parent.parent.parent.parent / "resources" / self.FOCUS_MODE_SHORTCUT_FILENAME)
+            path = str(Path(__file__).parent.parent.parent.parent / "resources"
+                       / f"{CommandExecutorConstants.MACOS_FOCUS_MODE_SHORTCUT_NAME}.shortcut")
         subprocess.check_call(["open", path])
         typer.prompt("Press Enter once you have installed the shortcut.", default='', prompt_suffix='\n')
 
