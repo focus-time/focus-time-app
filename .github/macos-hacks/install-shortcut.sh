40da43d4fb72a320d322f5b29a06beaa8ada05b5
MShekow <s2mashek@uni-bonn.de>
2023-06-29 10:28:01 +0200

    ci: another macos-test

--- .github/macos-hacks/install-shortcut.sh
+++ .github/macos-hacks/install-shortcut.sh
@@ -5,7 +5,7 @@ set -ex
 # Starts the Shortcuts app, it should show the dialog that asks the user to confirm installing the shortcut
 open resources/focus-time-app.shortcut
 
-sleep 10
+sleep 15
 
 # Clicks the button to install the shortcut
 osascript .github/macos-hacks/install-shortcut-unattended.scpt
