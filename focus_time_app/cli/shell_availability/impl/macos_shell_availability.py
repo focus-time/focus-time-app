761d5696a741178b231ceba8f04f97b2c3ea5f10
MShekow <s2mashek@uni-bonn.de>
2023-07-04 08:26:30 +0200

    fix: always refresh symbolic link on macOS

--- focus_time_app/cli/shell_availability/impl/macos_shell_availability.py
+++ focus_time_app/cli/shell_availability/impl/macos_shell_availability.py
@@ -1,24 +1,26 @@
 import os.path
 import sys
+from pathlib import Path
 
 from focus_time_app.cli.shell_availability import AbstractShellAvailability
 from focus_time_app.utils import is_production_environment
 
 
 class MacOsShellAvailability(AbstractShellAvailability):
-    BINARY_NAME = os.path.basename(sys.executable)
+    SYMLINK = Path("/usr/local/bin") / os.path.basename(sys.executable)
 
     def is_available(self) -> bool:
         if not is_production_environment():
             raise ValueError("Calling this method only makes sense for the frozen production binary")
 
-        return os.path.exists(f"/usr/local/bin/{self.BINARY_NAME}")
+        return self.SYMLINK.is_symlink() and self.SYMLINK.readlink() == Path(sys.executable)
 
     def make_available(self):
         if not is_production_environment():
             raise ValueError("Calling this method only makes sense for the frozen production binary")
 
-        os.symlink(src=sys.executable, dst=f"/usr/local/bin/{self.BINARY_NAME}")
+        self.SYMLINK.unlink(missing_ok=True)  # existing symbolic link might be outdated, so we always refresh it
+        self.SYMLINK.symlink_to(sys.executable)
 
     def requires_shell_restart(self) -> bool:
         return False
